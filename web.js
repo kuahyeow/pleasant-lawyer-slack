470703ac35e45a15bd829a573fb6ae4e8c2eeeec
Thong Kuah <kuahyeow@gmail.com>
2014-08-18 22:40:31 +1200

    Parse body

--- web.js
+++ web.js
@@ -1,19 +1,24 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var logfmt = require("logfmt");
 var app = express();
 PleasantLawyer = require('./pleasant-lawyer');
 var pleasantLawyer = new PleasantLawyer();
 
 app.use(logfmt.requestLogger());
+// parse application/json
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.all('/pleasant_lawyer', function(req, res) {
-  var inputText = req.query.text
+
+var inputTextToPL = function(inputText) {
   if(!inputText) {
-    res.send("text query parameter required")
     return
   }
 
@@ -22,13 +27,29 @@ app.all('/pleasant_lawyer', function(req, res) {
   if(number) {
     text = pleasantLawyer.numberToWords(number)
   }
+  return text
+}
 
+var sendToSlack = function(message, req, res) {
   if(req.accepts('application/json')) {
-    res.send({text: text})
+    res.send({text: message})
   } else {
     res.send(text)
   }
+}
+
+app.get('/pleasant_lawyer', function(req, res) {
+  var inputText = req.query.text
+  var text = inputTextToPL(inputText)
+  sendToSlack(text, req, res)
 })
+
+app.post('/pleasant_lawyer', function(req, res) {
+  var inputText = req.param('text')
+  var text = inputTextToPL(inputText)
+  sendToSlack(text, req, res)
+})
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
