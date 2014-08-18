a699e02ec94ee6d0937d30fad26eab5ca95b8d58
Thong Kuah <kuahyeow@gmail.com>
2014-08-18 22:04:27 +1200

    wow, pleasant_lawyer

---
+++ web.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+PleasantLawyer = require('./pleasant-lawyer');
+var pleasantLawyer = new PleasantLawyer();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/pleasant_lawyer', function(req, res) {
+  var inputText = req.query.text
+  if(!inputText) {
+    res.send("text query parameter required")
+    return
+  }
+
+  var number    = inputText.split(':')[1]
+  var text      = ""
+  if(number) {
+    text = pleasantLawyer.numberToWords(number)
+  }
+
+  if(req.accepts('application/json')) {
+    res.send({text: text})
+  } else {
+    res.send(text)
+  }
+})
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
